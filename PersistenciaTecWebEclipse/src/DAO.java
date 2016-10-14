b9a3e0e01f9d213391a2b86748c11e8de07ac08d
Carlos Rosa <kdu.carloseduardo@gmail.com>
2016-10-14 08:12:04 -0300

    Javabeans InformacoesPessoais e DadosPessoais adicionadas. Trabalhando na DAO.

--- PersistenciaTecWebEclipse/src/DAO.java
+++ PersistenciaTecWebEclipse/src/DAO.java
@@ -1,4 +1,34 @@
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+
 public class DAO {
+	private Connection connection = null;
+	public DAO() {
+	    try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    try {
+			connection = DriverManager.getConnection("jdbc:mysql://localhost/meus_dados", "root", "123456");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void adicionaInformacoesPessoais(InformacoesPessoais InformacaoPessoal){
+		
+	}
 
 }
---
+++ PersistenciaTecWebEclipse/src/DadosPessoais.java
@@ -0,0 +1,54 @@
+import java.util.Calendar;
+
+public class DadosPessoais {
+	private String nome;
+	private String pai;
+	private String sexo;
+	private String nacionalidade;
+	private String racaCor;
+	private String religao;
+	private Calendar nascimento;
+	private String mae;
+	private String estadoCivil;
+	private String naturalidade;
+	private String necessidadeEspecial;
+	
+	public String getNome() {return nome;}
+	public void setNome(String nome) {this.nome = nome;}
+	
+	public String getPai() {return pai;}
+	public void setPai(String pai) {this.pai = pai;}
+	
+	public String getSexo() {return sexo;}
+	public void setSexo(String sexo) {this.sexo = sexo;}
+	
+	public String getNacionalidade() {return nacionalidade;}
+	public void setNacionalidade(String nacionalidade) {
+		this.nacionalidade = nacionalidade;
+	}
+	
+	public String getRacaCor() {return racaCor;}
+	public void setRacaCor(String racaCor) {this.racaCor = racaCor;}
+	
+	public String getReligao() {return religao;}
+	public void setReligao(String religao) {this.religao = religao;}
+	
+	public Calendar getNascimento() {return nascimento;}
+	public void setNascimento(Calendar nascimento) {this.nascimento = nascimento;}
+	
+	public String getMae() {return mae;}
+	public void setMae(String mae) {this.mae = mae;}
+	
+	public String getEstadoCivil() {return estadoCivil;}
+	public void setEstadoCivil(String estadoCivil) {this.estadoCivil = estadoCivil;}
+	
+	public String getNaturalidade() {return naturalidade;}
+	public void setNaturalidade(String naturalidade) {this.naturalidade = naturalidade;}
+	
+	public String getNecessidadeEspecial() {return necessidadeEspecial;}
+	public void setNecessidadeEspecial(String necessidadeEspecial) {
+		this.necessidadeEspecial = necessidadeEspecial;
+	}
+	
+
+}
